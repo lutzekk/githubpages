b319758a1bbd22da6914f68c1e8e973b7e2f8548
lutzekk <63036002+lutzekk@users.noreply.github.com>
2024-09-15 23:52:24 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
-const coverElement = document.querySelector('.cover');
-const paperElement = document.querySelector('.paper');
-const heartElement = document.querySelector('.heart');
-
-// Función para abrir la carta al hacer clic
-coverElement.addEventListener('click', () => {
-  coverElement.classList.add('open-cover');
-  
-  setTimeout(() => {
-    coverElement.style.zIndex = -1;
-    paperElement.classList.remove('close-paper');
-    paperElement.classList.add('open-paper');
-    heartElement.style.display = 'block';
-  }, 500);
-});
+async function maubucin(){
+  await swals.fire({
+    title: '<strong>Mau script bucin yang lain?</strong>',
+    html:
+      'Cek scriptnya di ' +
+      '<a href="https://bit.ly/htmlbucin2022">https://bit.ly/scripthtmlbucin</a> ' +
+      'ya!' + '<br><br>' + 'Bisa diedit sesuka hatimu menggunakan aplikasi Quick Editor ataupun sejenisnya.',
+    focusConfirm: false,
+  });
+  window.location = "https://bit.ly/htmlbucin2022";
+  } 
+  async function maubucin2(){
+  await swals.fire({
+    title: '<strong>Mau mentahan script bucinnya?</strong>',
+    html:
+      'Ambil scriptnya di: ' +
+      '<a href="https://sfile.mobi/1KUBvuLh0k27">Klik di Sini!</a> ' +
+      'ya' + '<br><br>' + 'Bisa diedit sesukamu menggunakan aplikasi Quick Editor ataupun HTML Editor lainnya.',
+    focusConfirm: false,
+  });
+  window.location = "https://sfile.mobi/1KUBvuLh0k27";
+  }
